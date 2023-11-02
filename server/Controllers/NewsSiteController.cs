4c5001249bf58c23716daa2f12606d085a4388f8
jozsapeter88 <jozsapeter88@gmail.com>
2023-11-02 16:14:26 +0100

    create ScrapeWebsite controller method

--- server/Controllers/NewsSiteController.cs
+++ server/Controllers/NewsSiteController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using server.Models;
 using server.Services;
 
 [Route("api/news-sites")]
@@ -12,6 +15,25 @@ public class NewsSiteController : ControllerBase
         _newsSiteService = newsSiteService;
     }
 
+    private readonly Dictionary<string, (string titleXPath, string articleXPath)> websiteMappings =
+        new Dictionary<string, (string, string)>
+        {
+            {
+                "BBC",
+                (
+                    "//h1[@class='article-headline__text b-reith-sans-font b-font-weight-300']",
+                    "//div[@class='article__body-content']"
+                )
+            },
+            {
+                "Index",
+                (
+                    "//div[@class='content-title']",
+                    "//div[@class='cikk-torzs']"
+                )
+            },
+        };
+
     [HttpGet]
     [Route("getAllNewsSites")]
     public IActionResult GetAllNewsSites()
@@ -19,4 +41,61 @@ public class NewsSiteController : ControllerBase
         var newsSites = _newsSiteService.GetAllNewsSites();
         return Ok(newsSites);
     }
+
+    [HttpPost]
+    [Route("scrape")]
+    public IActionResult ScrapeWebsite([FromBody] ScrapeRequest request)
+    {
+        if (websiteMappings.TryGetValue(request.SelectedWebsite, out var xPathMappings))
+        {
+            if (!string.IsNullOrWhiteSpace(request.TitleXPath))
+            {
+                xPathMappings.titleXPath = request.TitleXPath;
+            }
+
+            if (!string.IsNullOrWhiteSpace(request.ArticleXPath))
+            {
+                xPathMappings.articleXPath = request.ArticleXPath;
+            }
+
+            try
+            {
+                var httpClient = new HttpClient();
+                var html = httpClient.GetStringAsync(request.Url).Result;
+                var htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(html);
+
+                var titleElement = htmlDocument.DocumentNode.SelectSingleNode(xPathMappings.titleXPath);
+                var articleElement = htmlDocument.DocumentNode.SelectSingleNode(xPathMappings.articleXPath);
+
+                if (titleElement != null && articleElement != null)
+                {
+                    var title = WebUtility.HtmlDecode(titleElement.InnerText);
+                    var article = WebUtility.HtmlDecode(articleElement.InnerText);
+
+                    var newsArticle = new NewsArticle
+                    {
+                        Title = title,
+                        Article = article
+                    };
+
+                    return Ok(newsArticle);
+                }
+                else
+                {
+                    return BadRequest("Title or article element not found on the page.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while scraping the website: " + ex.Message);
+            }
+        }
+        else
+        {
+            return BadRequest("Invalid website selection. Please choose a valid website.");
+        }
+    }
+
+
 }
