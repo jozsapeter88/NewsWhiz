9a3662775b8c31ece053fb09f0b7c93dde17d5fe
jozsapeter88 <jozsapeter88@gmail.com>
2023-11-02 16:07:59 +0100

    edit ScrapeRequest model

--- server/Models/ScrapeRequest.cs
+++ server/Models/ScrapeRequest.cs
@@ -3,5 +3,7 @@
 public class ScrapeRequest
 {
     public string SelectedWebsite { get; set; }
+    public string TitleXPath { get; set; }
+    public string ArticleXPath { get; set; }
     public string Url { get; set; }
 }
