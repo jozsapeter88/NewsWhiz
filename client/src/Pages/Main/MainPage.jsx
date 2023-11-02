b9458e22d78d886f5f96d91fcb358f91eb15ffa8
jozsapeter88 <jozsapeter88@gmail.com>
2023-11-02 16:05:04 +0100

    edit MainPage component to include basic user inputs

--- client/src/Pages/Main/MainPage.jsx
+++ client/src/Pages/Main/MainPage.jsx
@@ -1,33 +1,73 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 function MainPage() {
-  const [newsSites, setNewsSites] = useState([]);
+  const [selectedWebsite, setSelectedWebsite] = useState('');
+  const [titleXPath, setTitleXPath] = useState(''); // Custom XPath for title
+  const [articleXPath, setArticleXPath] = useState(''); // Custom XPath for article
+  const [url, setUrl] = useState('');
+  const [title, setTitle] = useState('');
+  const [article, setArticle] = useState('');
 
-  useEffect(() => {
-    // Make an HTTP GET request to the backend API using the fetch API
-    fetch('http://localhost:5092/api/news-sites/getAllNewsSites')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        setNewsSites(data);
-      })
-      .catch(error => {
-        console.error('Error fetching news sites:', error);
-      });
-  }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const requestData = {
+      selectedWebsite,
+      titleXPath,
+      articleXPath,
+      url,
+    };
+
+    // Send a POST request with requestData to the backend
+
+    // On the server side, use HtmlAgilityPack and requestData to scrape data
+
+    // Set the title and article state based on the scraped data
+  };
 
   return (
-    <div className="App">
-      <h1>News Sites</h1>
-      <ul>
-        {newsSites.map(newsSite => (
-          <li key={newsSite.id}>{newsSite.name}</li>
-        ))}
-      </ul>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Select a website:
+          <select value={selectedWebsite} onChange={(e) => setSelectedWebsite(e.target.value)}>
+            <option value="BBC">BBC</option>
+            <option value="Index">Index</option>
+            {/* Add options for other available websites */}
+          </select>
+        </label>
+        <br />
+        <label>
+          Custom XPath for Title:
+          <input
+            type="text"
+            value={titleXPath}
+            onChange={(e) => setTitleXPath(e.target.value)}
+            placeholder="//h1[@class='custom-title']" // Placeholder for guidance
+          />
+        </label>
+        <br />
+        <label>
+          Custom XPath for Article:
+          <input
+            type="text"
+            value={articleXPath}
+            onChange={(e) => setArticleXPath(e.target.value)}
+            placeholder="//div[@class='custom-article']" // Placeholder for guidance
+          />
+        </label>
+        <br />
+        <label>
+          Enter the URL to scrape:
+          <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} />
+        </label>
+        <br />
+        <button type="submit">Scrape</button>
+      </form>
+      <div>
+        <h2>Title: {title}</h2>
+        <p>Article: {article}</p>
+      </div>
     </div>
   );
 }
