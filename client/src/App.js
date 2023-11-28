04bd996bfe0306e3551ce4722df7ac083b16cc93
jozsapeter88 <jozsapeter88@gmail.com>
2023-11-28 20:10:22 +0100

    add LoginPage/Signin component

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import React from "react";
 import MainPage from "./Pages/Main/MainPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RegisterPage from "./Pages/Main/RegisterPage/RegisterPage";
+import LoginPage from "./Pages/Main/LoginPage/LoginPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
---
+++ client/src/Pages/Main/LoginPage/LoginPage.jsx
@@ -0,0 +1,60 @@
+import React, { useState} from "react";
+import SignIn from "../SignIn/SignIn";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../Contexts/AuthContext";
+
+const LoginPage = () => {
+    const navigate = useNavigate();
+    const [showMsg, setShowMsg] = useState(true)
+    const [rememberMe, setRememberMe] = useState(false);
+    const { login } = useAuth();
+    
+    const handleRememberMeChange = (e) => {
+        setRememberMe(e.target.checked);
+    };
+    const onSubmit = async (e) => {
+        setShowMsg(true);
+        e.preventDefault();
+        sessionStorage.clear()
+        
+        const user ={
+            userName: e.target.formBasicUsername.value,
+            passWord: e.target.formBasicPassword.value,
+            rememberMe: rememberMe
+        }
+        try {
+            const response = await fetch(process.env.REACT_APP_API_URL + '/user/login', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              credentials: "include",
+              body: JSON.stringify( user),
+            });
+      
+            if (response.ok) {
+                const contentType = response.headers.get('content-type');
+                if (contentType && contentType.includes('application/json')) {
+                  const userData = await response.json();
+                  login(userData);
+                  navigate(`/facilities/${userData.id}`);
+                } else {
+                  console.error('Login response is not JSON');
+                }
+              } else {
+                console.error('Login failed');
+              }
+            } catch (error) {
+              console.error('Error logging in:', error);
+            }
+          };
+    
+    return (
+    <SignIn 
+    onSubmit={onSubmit} 
+    showMsg={showMsg} 
+    rememberMe={rememberMe}
+    handleRememberMeChange={handleRememberMeChange}></SignIn>)
+}
+
+export default LoginPage;
