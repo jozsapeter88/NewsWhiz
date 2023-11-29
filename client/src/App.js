6debc4a14bf73b576871f44929962e3d1761463b
jozsapeter88 <jozsapeter88@gmail.com>
2023-11-29 15:43:25 +0100

    add role based authentication in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,33 @@
 import "./App.css";
+import { AuthContextProvider, useAuth } from "./Contexts/AuthContext";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import React from "react";
 import MainPage from "./Pages/Main/MainPage";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import LoginPage from "./Pages/Main/LoginPage/LoginPage";
 import RegistrationForm from "./Pages/Main/RegisterPage/RegisterPage";
 
 function App() {
+  const { user, login } = useAuth();
+  useEffect(() => {
+    const userJSON = sessionStorage.getItem("user");
+    if (userJSON) {
+      const userData = JSON.parse(userJSON);
+      login(userData);
+    }
+  }, []);
+
+  const isAuthenticated = (allowedRoles) => {
+    if (user === null) return false;
+    return allowedRoles.includes(user.role);
+  };
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route
+          path="/"
+          element={isAuthenticated([0, 1]) ? <MainPage /> : <LoginPage />}
+        />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegistrationForm />} />
       </Routes>
@@ -18,4 +35,10 @@ function App() {
   );
 }
 
-export default App;
+const AppWithAuthContext = () => (
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+);
+
+export default AppWithAuthContext;
