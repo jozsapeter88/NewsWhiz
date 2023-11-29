048d23352ceb03b909c992ca970870f2fde935bb
jozsapeter88 <jozsapeter88@gmail.com>
2023-11-29 15:54:24 +0100

    register user service, identity role and add admin user

--- server/Program.cs
+++ server/Program.cs
@@ -4,6 +4,7 @@ using server.Models.DBContext;
 using server.Services;
 using Microsoft.AspNetCore.Identity;
 using server.Areas.Identity.Data.Models;
+using server.Areas.Identity.Enum;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
@@ -25,11 +26,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-
-builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentity<User, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddTransient<INewsSiteService, NewsSiteService>();
+builder.Services.AddTransient<IUserService, UserService>();
 
 var app = builder.Build();
 
@@ -40,6 +41,30 @@ using (var serviceScope = app.Services.CreateScope())
     if (context.Database.EnsureCreated())
     {
         DataSeed.Initialize(context);
+    }
+}
+
+//Add adminUser
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    if (!await roleManager.RoleExistsAsync("Admin"))
+    {
+        var adminRole = new IdentityRole("Admin");
+        await roleManager.CreateAsync(adminRole);
+    }
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+    var adminUser = new User
+    {
+        UserName = "admin",
+        Email = "admin@example.com",
+        Role = RoleEnum.Admin
+    };
+
+    var result = await userManager.CreateAsync(adminUser, "Admin1234!");
+    if (result.Succeeded)
+    {
+        await userManager.AddToRoleAsync(adminUser, "Admin");
     }
 }
 
